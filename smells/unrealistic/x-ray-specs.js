f86569b7cf352f66a7e51411e35c8440be26ce8e
Vasily Polovnyov <vasily@polovnyov.ru>
2016-10-12 20:03:39 +0300

    translate x-ray-specs

--- smells/unrealistic/x-ray-specs.js
+++ smells/unrealistic/x-ray-specs.js
@@ -1,25 +1,20 @@
 /* Рентгеновские тесты
  *
- * In this example, you can see the test is routinely exploiting its ability
- * to access private variables on the subject to do its assertions (when the
- * prudent thing to do would be to design a public API so that the behavior
- * of the subject could be asserted without looking at its internal state).
- * However, the last test requirement takes this to a new extreme: intentionally
- * mucking with the state of the subject's cache of approved seat changes to
- * ensure that the subject isn't making an approval call unnecessarily. It makes
- * sense within the context of the test (where the author is already comfortable
- * with accessing the subject's internal state), but upon introspection a better
- * strategy would be to test whether the interaction between `SeatMap` and the
- * approval API takes place in a more direct, less ambiguous way.
+ * В примере ниже тест использует доступ к приватным переменным для проверок.
+ * Хотя разумнее было бы сделать публичный АПИ так, чтобы поведение
+ * объекта можно было проверить, не заглядывая в его внутреннее состояние.
  *
- * [Note: Because it's hard to demonstrate changing the visibility of, say, an
- * anonymous function to one that's exported in a single file example, the
- * following example uses a `__` prefix to denote variables intended to be
- * private from users of the object.]
+ * Последний тест — жесть. Намеренно портит состояние кэша подтвержденных
+ * мест, чтобы убедиться что нет дополнительного вызова функции, подтверждающей
+ * места. Это имеет смысл в контексте теста, где автор уже закрыл глаза
+ * на использование приватных переменных в проверках, но лучше было бы
+ * явно проверить взаимодействие между `SeatMap` и АПИ подтверждения.
  *
- * [Note 2: Remember, privacy is not about what's technically reachable, it's
- * what the author intends to prevent others from depending on so that the
- * implementation can be freely changed in the future!]
+ * [Пример использует префикс `__`, чтобы обозначить приватные переменные.]
+ *
+ * [Приватность — это не про видимость функций и методов. А о том, от чего
+ * не стоит зависеть в своем коде. Приватные функции и методы нестабильны:
+ * изменятся или исчезнут в будущем.]
  */
 
 // Тестируемый модуль
